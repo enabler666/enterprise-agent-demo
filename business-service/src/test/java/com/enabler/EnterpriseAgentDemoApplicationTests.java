7e0b4cf5ad786c7ba13176efac2546a92986da37
Enabler666 <1193026400@qq.com>
2026-07-09 01:10:11 +0800

    init

---
+++ business-service/src/test/java/com/enabler/EnterpriseAgentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enabler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAgentDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
